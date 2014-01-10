3322fd2f858795ee4fec0cad017f73d70e2f354e
micol972@gmail.com <micol972@gmail.com@1f0e9155-ede9-3f68-dca1-82ebe1e4ca03>
2014-01-10 09:47:59 +0000

    Moved some more target functions into separate files

---
+++ targets/target_sgg.go
@@ -0,0 +1,132 @@
+/*
+ * Package targets
+ * Target SMS (Sega Game Gear)
+ *
+ * Part of XPMC.
+ * Contains data/functions specific to the SGG output target
+ *
+ * /Mic, 2012-2014
+ */
+ 
+package targets
+
+import (
+    "fmt"
+    "os"
+    "time"
+    "../specs"
+    "../utils"
+)
+
+
+/* Sega Gamegear *
+ *****************/
+
+func (t *TargetSGG) Init() {
+    utils.DefineSymbol("SGG", 1)
+    
+    specs.SetChannelSpecs(&t.ChannelSpecs, 0, 0, specs.SpecsSN76489)    // A..D
+    
+    t.ID                = TARGET_SGG
+    t.MaxTempo          = 300
+    t.MinVolume         = 0
+    t.SupportsPanning   = 1
+    t.MaxLoopDepth      = 2
+    t.MachineSpeed      = 3579545
+}
+
+
+func (t *TargetSGG) Output(outputVgm int) {
+    fmt.Printf("TargetSGG.Output\n")
+
+    fileEnding := ".asm"
+    if outputVgm == 1 {
+        fileEnding = ".vgm"
+    } else if outputVgm == 2 {
+        fileEnding = ".vgz"
+    }
+
+    if outputVgm != 0 {
+        // ToDo: output VGM/VGZ
+        return
+    }
+  
+    outFile, err := os.Create(t.CompilerItf.GetShortFileName() + fileEnding)
+    if err != nil {
+        utils.ERROR("Unable to open file: " + t.CompilerItf.GetShortFileName() + fileEnding)
+    }
+
+    now := time.Now()
+    outFile.WriteString("; Written by XPMC on " + now.Format(time.RFC1123) + "\n\n")
+
+    outFile.WriteString(".DEFINE XPMP_GAME_GEAR\n")
+
+    palNtscString := ".db 0"
+    t.MachineSpeed = 3579545
+
+    // Output the SGC header
+    outFile.WriteString(
+    ".IFDEF XPMP_MAKE_SGC\n\n" +
+    ".MEMORYMAP\n" +
+    "\tDEFAULTSLOT 1\n" +
+    "\tSLOTSIZE $4000\n" +
+    "\tSLOT 0 $0000\n" +
+    "\tSLOT 1 $4000\n" +
+    ".ENDME\n\n")
+    outFile.WriteString(
+    ".ROMBANKSIZE $4000\n" +
+    ".ROMBANKS 2\n" +
+    ".BANK 0 SLOT 0\n" +
+    ".ORGA $00\n\n")
+
+    systemType := 0     // SMS
+    if !t.SupportsPal {
+        systemType = 1  // GG
+    }
+    outFile.WriteString(
+    ".db \"SGC\"\n" +
+    ".db $1A\n" +
+    ".db 1\t\t; Version\n" +
+    palNtscString + "\n" + 
+    ".db 0, 0\n" +
+    ".dw $0400\t; Load address\n" +
+    ".dw $0400\t; Init address\n" +
+    ".dw $0408\t; Play address\n" +
+    ".dw $dff0\t; Stack pointer\n" +
+    ".dw 0\t\t; Reserved\n" +
+    ".dw $040C\t; RST 08\n" +
+    ".dw $040C\t; RST 10\n" +
+    ".dw $040C\t; RST 18\n" +
+    ".dw $040C\t; RST 20\n" +
+    ".dw $040C\t; RST 28\n" +
+    ".dw $040C\t; RST 30\n" +
+    ".dw $040C\t; RST 38\n" +
+    ".db 0, 0, 1, 2\t; Mapper setting (none)\n" +
+    ".db 0\t\t; Start song\n" +
+    fmt.Sprintf(".db %d\t\t; Number of songs\n", len(t.CompilerItf.GetSongs())) +
+    ".db 0, 0\t; Sound effects (none)\n" +
+    fmt.Sprintf(".db %d\t\t; System type\n", systemType) +
+    ".dw 0,0,0,0,0,0,0,0,0,0,0 ; Reserved\n" +
+    ".db 0")
+
+    outputStringWithExactLength(outFile, t.CompilerItf.GetSongs()[0].GetTitle(), 32)
+    outputStringWithExactLength(outFile, t.CompilerItf.GetSongs()[0].GetComposer(), 32)
+    outputStringWithExactLength(outFile, t.CompilerItf.GetSongs()[0].GetProgrammer(), 32)  
+    
+    outFile.WriteString(".INCBIN \"sgc.bin\"\n\n")
+    outFile.WriteString(".ELSE\n\n") 
+    
+    t.outputEffectFlags(outFile, FORMAT_WLA_DX)
+         
+    tableSize := outputStandardEffects(outFile, FORMAT_WLA_DX)  
+    outFile.WriteString("\n")
+    utils.INFO("Size of effect tables: %d bytes", tableSize)
+
+    patSize := t.outputPatterns(outFile, FORMAT_WLA_DX)
+    utils.INFO("Size of patterns table: %d bytes\n", patSize)
+ 
+    songSize := t.outputChannelData(outFile, FORMAT_WLA_DX) 
+    utils.INFO("Total size of song(s): %d bytes", songSize + tableSize)
+
+    outFile.Close()    
+}
